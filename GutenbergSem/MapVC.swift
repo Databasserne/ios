7492cbdc77a5792fc7c3a03c02328c4d7a4ce17a
Jonas Simonsen <jks.ufo@icloud.com>
2017-05-17 14:35:30 +0200

    Added MapVC

---
+++ GutenbergSem/MapVC.swift
@@ -0,0 +1,27 @@
+//
+//  MapVC.swift
+//  GutenbergSem
+//
+//  Created by Jonas Simonsen on 17/05/2017.
+//  Copyright © 2017 Databasserne. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func geoOnMap() {
+        
+    }
+}
