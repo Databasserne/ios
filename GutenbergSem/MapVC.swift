487e3e35ac84f700d4cac0d9fd55cd4a6e8e0838
Jonas Simonsen <jks.ufo@icloud.com>
2017-05-21 12:51:38 +0200

    Added annotations.

--- GutenbergSem/MapVC.swift
+++ GutenbergSem/MapVC.swift
@@ -7,21 +7,34 @@
 //
 
 import UIKit
+import MapKit
 
 class MapVC: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let locations = [
+            ["title": "New York, NY",    "latitude": 40.713054, "longitude": -74.007228],
+            ["title": "Los Angeles, CA", "latitude": 34.052238, "longitude": -118.243344],
+            ["title": "Chicago, IL",     "latitude": 41.883229, "longitude": -87.632398]
+        ]
+        
+        for location in locations {
+            let annotation = MKPointAnnotation()
+            annotation.title = location["title"] as? String
+            annotation.coordinate = CLLocationCoordinate2D(latitude: location["latitude"] as! Double, longitude: location["longitude"] as! Double)
+            mapView.addAnnotation(annotation)
+        }
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func geoOnMap() {
-        
-    }
 }
+
